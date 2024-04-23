00c2e7f7507ad3a6743b7c10dface388bdfcca10
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-23 16:57:27 +0400

    react-slideshow-image slider added with zoom

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -1,12 +1,49 @@
+"use client"
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from "react-icons/io";
+import { Zoom } from "react-slideshow-image";
+import 'react-slideshow-image/dist/styles.css'
 import Image from 'next/image'
 import React from 'react'
 import HeaderNav from './HeaderNav'
 
 export default function HomeSlider() {
+  const images = [
+    "/learn-machine-learning.webp",
+    "/it-training-in-dubai.webp",
+    "/summer-course-banner-1.jpg",
+]
+const zoomInProperties = {
+  scale:1, // how large image can transform
+  duration:5000, // image change duration
+  transitionDuration:300, // transition per image duration
+  infinity:true, //loop the transition infinity
+
+  //icons
+  prevArrow:(
+      <div className="ml-10 top-40 md:top-72">
+          <IoIosArrowDropleftCircle className="h-8 w-8 text-white cursor-pointer" />
+      </div>
+  ),
+  nextArrow:(
+      <div className="mr-10 top-40 md:top-72">
+          <IoIosArrowDroprightCircle className="h-8 w-8 text-white cursor-pointer" />
+      </div>
+  ),
+}
   return (
     <div className='flex flex-col relative'>
         <HeaderNav />
-        <Image src={"/learn-machine-learning.webp"} width={1903} height={700} className='object-fill' />
+        <div className="w-full h-screen">
+        <Zoom {...zoomInProperties}>
+            {images.map((each, index) => (
+                <div key={index} className="flex justify-center md:items-center items-start w-screen h-screen relative">  
+                <Image className="w-screen object-fill" src={each} width={1920} height={766} />
+                <h1 className="absolute md:top-60 top-24 -inset-x-1/4 text-center z-10 md:text-6xl text-4xl bold text-white">Welcome</h1>
+                <p className="absolute md:top-70 top-36 -inset-x-1/4 text-center z-10 md:text-4xl text-2xl bold text-white">Welcome</p>
+                </div>
+            ))}
+        </Zoom>
+    </div>
     </div>
   )
 }
