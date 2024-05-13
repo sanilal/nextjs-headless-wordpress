8b1bbba7223b9612496abe2503d366030b43cd77
Sanilal P Sathyan <sanilal@gmail.com>
2024-05-13 18:05:59 +0400

    about updated

---
+++ src/app/about-aptech/page.jsx
@@ -0,0 +1,24 @@
+export const metadata = {
+    title: 'About Page',
+    description: 'About description',
+  }
+
+export default function About() {
+  return (
+    <div>
+      <h1>About APTECH</h1>
+      <div className="flex flex-col">
+      <div className="">
+        <img src="https://www.aptech.ae/wp-content/uploads/2020/12/aptech-lab-2.jpg" alt="" />
+      </div>
+      <div className="">
+        <p>Aptech Computer Training is among the leaders in career education in the UAE and provides ministry approved certificate courses.
+The company focuses on career development for students and working professionals. In addition, Aptech also offers various training services to large companies to help develop the skills of their employees.
+Newly emerging trends and technologies like Artificial Intelligence (AI) cloud computing, mobile applications, Multimedia courses, Social media and Data analytics are bringing about increasing job opportunities in the IT & ICT industry. Aptech Computer Training, through its courses and curriculum, ensures that you are at par with global standards. </p>
+
+<p>Aptech Computer Training is not related to Aptech Worldwide</p>
+      </div>
+      </div>
+       </div>
+  )
+}
